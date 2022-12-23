7e9cc566b27518c57f983e49af42f35b295c2a1c
bank <sirawit.36298@gmail.com>
2022-12-23 15:33:24 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double n){
+	if(n>90){
+		return 'A';
+	}else if(n>75){
+		return 'B';
+	}else if(n>60){
+		return 'C';
+	}else if(n>45){
+		return 'D';
+	}else{
+		return 'F';
+	}
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +31,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
